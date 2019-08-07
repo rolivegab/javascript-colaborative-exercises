ec7fd8c9441f6e7241441e1e83470894b293daa8
kimferrari <53798783+kimferrari@users.noreply.github.com>
2019-08-06 23:57:05 -0300

    segunda resposta do exercício 2

---
+++ src/2/kimferrari2.js
@@ -0,0 +1,11 @@
+export default function (num) {
+    function teste2(n) {
+        var contador = n
+        while (contador > 0) {
+            console.log ("hidrocor");
+            contador = contador - 1
+        }
+
+    teste2(num)
+    } 
+}
